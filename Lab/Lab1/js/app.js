4795be7bafded59fdaa732ede3646d03bf20f004
Grace22222222 <gtpayne@iu.edu>
2022-01-18 16:38:32 -0500

    Finished Personal Composition

--- Lab/Lab1/js/app.js
+++ Lab/Lab1/js/app.js
@@ -1,9 +1,42 @@
+//Grace Payne  
+//1/18/2022
+//N220
 
-function setup() {
-    createCanvas(800,600)
+//Setting up Canvas
+    function setup() {
+     createCanvas(600,600);
 }
 
-function draw () {
-    fill (255,0,0);
-    circle(mouseX,mouseY,frameCount/50)
-}
+//Drawing and coloring circle
+   
+    function draw() {
+
+        circle( 300, 300, 900);
+            /*fill(7, 34, 39)*/
+            /*fill(135, 100, 69);*/
+            fill(174, 254, 255);
+
+
+//Drawing and coloring another circle
+
+
+        circle( 100, 100, 70);
+            /*fill(174, 254, 255);*/
+            fill(135, 100, 69);
+
+
+
+//Drawing and coloring square
+    
+
+        square( 0, 500, 600);
+           /* fill(135, 100, 69)*/
+            /*fill(7, 34, 39);*/
+           /* fill(174, 254, 255);*/
+            fill(7, 34, 39);
+
+//Lots of removing colors. For some reason the colors kept
+        //switching each other
+
+    }
+
