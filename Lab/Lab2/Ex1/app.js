14ee332e3d9ba831a41c2f0395b301394a926a56
Grace22222222 <gtpayne@iu.edu>
2022-01-20 11:38:56 -0500

    Finished Puck Side

--- Lab/Lab2/Ex1/app.js
+++ Lab/Lab2/Ex1/app.js
@@ -5,6 +5,70 @@
 //Puck Side
 
 
+//Canvas Creation
+
+
+
 function setup() {
-    createCanvas(400,300)
+    createCanvas(400,300);
 }
+
+
+//Circle Creation
+
+function draw() {
+
+    circle(mouseX,mouseY,50);
+    if (mouseX >200) {
+
+        fill(255,0,0);
+        
+    }
+    if (mouseX <200) {
+
+        fill(0,0,255);
+    }
+
+}
+
+
+
+
+/*
+
+//IN CLASS WORK
+
+let size = 10;
+
+//Canvas Set up
+
+    function setup() {
+
+        createCanvas(500,500);
+
+    }
+
+
+//Circle 
+
+    function draw () {
+
+        circle(mouseX,mouseY,10);
+
+    }
+    
+//Sizing
+
+    function draw () {
+
+        
+        size = size+15;
+        console.log(size);
+
+        circle (mouseX,mouseY,size)
+
+    }
+
+//If Statment
+
+*/
