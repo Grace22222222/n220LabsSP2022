ef46b1435cb8da2e969965788c16ce2735ae11dd
Grace22222222 <gtpayne@iu.edu>
2022-02-03 11:31:37 -0500

    Updated to clean up

--- Lab/ObEX/ObEx1/app.js
+++ Lab/ObEX/ObEx1/app.js
@@ -8,6 +8,8 @@
 
 //Make an object with all the properties for a graphic on the screen - width, height, color, position. Perhaps more. In your create or draw function draw that object to the screen using the data in your object. Then, create a function that takes one argument and draws an object to the screen using that argument. Pass your object to that function as an argument, and have the function draw the object to the screen
 
+//Setup
+
 function setup() {
 
     createCanvas(500,500);
