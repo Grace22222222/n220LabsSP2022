7fb4e34264d5b2b1a1b52e15a191c134f2c3f87d
Grace22222222 <gtpayne@iu.edu>
2022-02-06 15:24:04 -0500

    Started setup

--- Lab/ObEX/ObEx3/app.js
+++ Lab/ObEX/ObEx3/app.js
@@ -8,5 +8,16 @@
 
 //Take your ball bounce assignment and recode it using an object to store the ball's size, color, and velocity. No global variables or hardcoded values should be used in your update function.
 
+//BALL BOUNCE INSTRUCTIONS
+
+//Make a canvas 800 px x 600 px     Create a sketch with a circle moving left and down at 5px per update. This speed should be stored as separate speed variables in your code (this is required to solve this problem).    Assume a 'world bounds' for this object to be 800x600   When the ball reaches the edge of the bounds, bounce it by multiplying the associated velocity variable by *= -1, to make the ball be moving in the opposite direction. If you have found a hit on the right or left bounds, it should be the x velocity variable you are modifying. If its on the top or bottom bounds, you should be modifying the y velocity variable.
+
 //Setup
+
+function setup() {
+
+    createCanvas(800,600)
+
+
+}
 
