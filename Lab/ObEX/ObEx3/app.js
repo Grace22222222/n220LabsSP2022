c0b1b82b66d85ec8ba4f3e2603e58c1df834fe73
Grace22222222 <gtpayne@iu.edu>
2022-02-08 18:09:31 -0500

    Finished World Wrap Remake

--- Lab/ObEX/ObEx3/app.js
+++ Lab/ObEX/ObEx3/app.js
@@ -2,27 +2,23 @@
 //2/3/2022
 //N220
 
-//Bouncing Ball Refactor
+//World Wrap Refactor
 
 //INSTRUCTIONS
 
-//Take your ball bounce assignment and recode it using an object to store the ball's size, color, and velocity. No global variables or hardcoded values should be used in your update function.
+//Take your ball bounce assignment or world wrap and recode it using an object to store the ball's size, color, and velocity. No global variables or hardcoded values should be used in your update function.
 
-//BALL BOUNCE INSTRUCTIONS
-
-//Make a canvas 800 px x 600 px     Create a sketch with a circle moving left and down at 5px per update. This speed should be stored as separate speed variables in your code (this is required to solve this problem).    Assume a 'world bounds' for this object to be 800x600   When the ball reaches the edge of the bounds, bounce it by multiplying the associated velocity variable by *= -1, to make the ball be moving in the opposite direction. If you have found a hit on the right or left bounds, it should be the x velocity variable you are modifying. If its on the top or bottom bounds, you should be modifying the y velocity variable.
+//World Wrap for example
 
 
+
+//let xPos=10;
 
 //Setup Object and Start Position
 
-//let xPos= 50;
-
-//let yPos= 50;
-
 let objBall ={
 
-    x:50, y:50, ballColor:"99FF33", size:25
+    x:10, y:50, ballColor:"#99FF33", size:40
 
 } ;
 
@@ -31,18 +27,39 @@ let objBall ={
 
 function setup() {
 
-    createCanvas(800,600)
-
-    fill(objBall.ballColor);
-    circle(objBall.x, objBall.y, objBall.size);
+    createCanvas(800,600);
 
 }
 
+//Draw
+
 function draw() {
 
-    //background(255);
-    x=50 +5;
-    y=50 -5;
+    background(255);
 
+    circle(objBall.x, objBall.y, objBall.size);
+    
+    fill(objBall.ballColor);
+
+    objBall.x=objBall.x + 5;
+
+        if (objBall.x==800){
+
+            objBall.x=10;
+
+        }
+
+
+//Tried this out with position instead, yet it wouldn't work with the object functions. Not sure how to get it to work, as I know it is possible, yet I couldn't figure it out. I found another way to get it to work however
+
+    /*xPos=xPos+5;
+
+    
+        if (xPos==800){
+
+            xPos=10;
+
+        }
+*/
 }
 
