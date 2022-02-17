45cf612335ad9dc795881e5925cefeb35d98fce9
Grace22222222 <gtpayne@iu.edu>
2022-02-17 17:44:03 -0500

    Finished mcDiv'ns

--- Lab/DocObManEx/DocObManEx2/app.js
+++ Lab/DocObManEx/DocObManEx2/app.js
@@ -8,29 +8,39 @@
 
 let clown = document.getElementById("ronald");
 
-var burg="clowns";
-
+var ger="clowns";
+var bur= "mc";
+var orderNum=0;
 
 
 ronald.style.height="100";
 ronald.style.width="100";
+
+//Used color to make sure the div was working properly
 //ronald.style.backgroundColor="#FFFF55";
 
-ronald.innerHTML="clowns";
+ronald.innerHTML=ger;
 
 function fry() {
+
+    orderNum=orderNum+1;
+    ronald.innerHTML=bur+ronald.innerHTML;
+
+    if (orderNum>2) {
+
+        ronald.innerHTML=ger+ger+ger;
+
+    }
+    //I believe I did this correctly
 
     //Take what's in it
     //Store it
     //Add 'Mc' to the variable
     //Add to document
-    ronald.innerHTML="mcclowns";
     //["mc" + clowns]
 
-    console.log(mcclowns);
-
+    console.log(orderNum);
 }
 
-
-
 ronald.onclick = function() {fry()};
+//I'm surprised I managed to stick with the whole fast food thing
