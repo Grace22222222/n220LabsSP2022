cfebe559b4c0cf19230ec8b57230095226f73195
Grace22222222 <gtpayne@iu.edu>
2022-02-22 11:30:49 -0500

    Did classwork

--- Lab/LoopArrayEx/LoopArrayEx1/app.js
+++ Lab/LoopArrayEx/LoopArrayEx1/app.js
@@ -16,3 +16,13 @@ for(i=0;i<5;i++) {
 }
 
 //I'm not sure if I did this entirely correctly or not. It'll do for now I suppose
+
+
+//Classwork
+
+/*
+Create a circle with a radius of 40 that follows the mouse. If it's on the right side of the screen, make it grow by .3px. If it's on the left side of the screen, make it shrink by .3px. Also make it have different colors depending on the side of the screen, the colors being your choice.
+
+ Skills you would need: Mousex, Mousey, global variable, draw, array
+
+ */
