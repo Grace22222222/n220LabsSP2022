e2c16c953a2dea05d0059f08530b9d67dbe0f19d
Grace22222222 <gtpayne@iu.edu>
2022-02-22 18:49:27 -0500

    Did some work

--- Lab/LoopArrayEx/LoopArrayEx2/app.js
+++ Lab/LoopArrayEx/LoopArrayEx2/app.js
@@ -8,3 +8,16 @@
 //Write a loop that runs 25 times and writes the iterand (current iteration of the loop) out onto the page inside of a div element.However, When the iterand is divisible by 3, write the word "beep" instead/ When the iterand is divisible by 5, write the word "bop" instead/ When the iterand is divisible by 3 AND 5, write "beepbop" instead/
 
 let robit = document.getElementById("Robot");
+
+for(let i=0;i<26;i++){
+
+    robit.innerHTML+= i + "&nbsp;";
+
+}
+
+if (variable=4) {
+
+hhhh
+
+}
+
