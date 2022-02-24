ad6e3e0093fde46ab127d4898f847814b479dbc0
Grace22222222 <gtpayne@iu.edu>
2022-02-24 11:48:18 -0500

    Finished Favvorray and almost finished Runner Up

--- Lab/LoopArrayEx/LoopArrayEx3/app.js
+++ Lab/LoopArrayEx/LoopArrayEx3/app.js
@@ -10,7 +10,7 @@ let Sonic = document.getElementById("Speed");
 
 //Best times: 10:30, 12:00, 2:40
 
-let times= ["10:30", "12:00", "2:40"];
+let times= [8.50, 10.30, 12.1];
 
 for (let i=0; i<times.length;i++) {
 
@@ -18,5 +18,47 @@ for (let i=0; i<times.length;i++) {
 
 }
 
-//Write code so times go in order from lowest to highest
-//Show second and third place times in different div's
+//Write code so times go in order from lowest to highest (Already did that)
+
+//Show second and third place times in different div's (Not a loop)
+
+
+
+//CLASSWORK 
+/*
+
+let res=numCount([30,19,31,2,9,22]);
+console.log(res);
+
+
+function numCount (numbers) {
+
+
+    var count=0;
+
+    for(var i=0;i<numbers.length;i++) {
+
+        
+
+        if (numbers[i] > 20) {
+
+            count= count+1
+
+        } 
+            console.log(count);
+    }
+
+    if(count<= 3) {
+
+        return count;
+
+    }
+
+    if(count> 3) {
+
+        return -1;
+
+    }
+
+
+} */
--- Lab/LoopArrayEx/LoopArrayEx4/app.js
+++ Lab/LoopArrayEx/LoopArrayEx4/app.js
@@ -6,4 +6,13 @@
 
 let Flapflap = document.getElementById("SeaPancake");
 
-//I don't know what to do
+//Write five favorite things
+//Favorite things: Cats, sushi, games, apple orchards, and Autumn
+
+let things=["Cats","Sushi","Games","Apple Orchards","Autumn"];
+
+for (let i=0;i<things.length;i++) {
+
+    Flapflap.innerHTML+= things[i] + "&nbsp;" + "is one of my favorite things" +"&nbsp;";
+
+}
