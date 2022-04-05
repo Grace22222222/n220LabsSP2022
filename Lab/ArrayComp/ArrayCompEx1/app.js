84d8fce76b91dd71b869c75529e295c121cc6eac
Grace22222222 <gtpayne@iu.edu>
2022-04-05 11:47:25 -0400

    Started work on array Comps

--- Lab/ArrayComp/ArrayCompEx1/app.js
+++ Lab/ArrayComp/ArrayCompEx1/app.js
@@ -4,3 +4,69 @@
 
 
 //It's just average
+
+//Takes a list of comma seprarated numbers
+//Splits list into an array of numbers
+//Converts the string version of the numbers into number versions
+//Loops through the array and sums up the values
+//Calculates the average of the array
+//Displays the average and the sum to the user
+
+let averaType = document.getElementById("averaType");
+let numberIn = document.getElementById("numberIn");
+averaType.innerHTML = "Type some numbers! Be sure to put commas between them!";
+
+function calcuNum() {
+
+    let numType = numberIn.value;
+
+    numberIn.value = "";
+
+    //var res = StrVariableName.split(",");
+
+}
+
+numberIn.value = "";
+
+/*
+function arrayMaker() {
+
+    let numType = numberIn.value;
+
+
+
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//CLASSWORK
+/*
+let scores = [3,4,9,12,55,20];
+let res =scores.filter(score => score % 2 ==0);
+console.log(res);
+
+
+var grid =[
+
+    ["x","o","o"]
+    ["o","x","o"]
+    ["x","x","o"]
+]
+
+let res = grid["x","x","x"]["o","o","o"];
+console.log=(res);
+*/
