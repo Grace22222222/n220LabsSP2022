263d4e1ef83f6b59f26b516c9b6803ccae4ec91a
Grace22222222 <gtpayne@iu.edu>
2022-04-05 21:28:57 -0400

    Attempted work

--- Lab/ArrayComp/ArrayCompEx2/app.js
+++ Lab/ArrayComp/ArrayCompEx2/app.js
@@ -4,3 +4,17 @@
 
 
 //Make Divs
+
+//Given the array 
+
+let objects = [
+
+ { color: "#FF0000", height: 100, width: 300 },
+
+ { color: "#FFFF00", height: 200, width: 200 },
+
+ { color: "#ff0000", height: 300, width: 100 },
+
+];
+
+//Write a loop that crates 3 divs based on the data. Should only hae one document.createElement in code
--- Lab/ArrayComp/ArrayCompEx3/app.js
+++ Lab/ArrayComp/ArrayCompEx3/app.js
@@ -4,3 +4,27 @@
 
 
 //Check Win Function
+
+/*
+
+Create an application that: 
+
+Takes input from the user in a text field
+Assumes the input is a set of comma-separated values
+Splits the input into an array
+Uses a loop to check to see if there are three "1s" in a row in the array
+Displays the word "winner" or "not winner" on the document depending on the result
+
+Check Win Function
+
+Taking the above problem description, change the interaction to employ a function to check to see if there is a winner. The function should
+
+
+Accept an array of numbers
+Loop through the array checking for the win condition
+Return true if a win is found, and false is if is not found
+ 
+
+Use this function to modify the rest of your application to employ it.
+
+*/
