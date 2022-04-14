c9e252b76b97f3b996442ecaddad7bab99b923c9
Grace22222222 <gtpayne@iu.edu>
2022-04-14 11:56:22 -0400

    Almost finished assignment

--- Lab/EventsEx/EventsEx2/app.js
+++ Lab/EventsEx/EventsEx2/app.js
@@ -12,7 +12,10 @@
 
 let mixBox = document.getElementById("mixBox");
 
+let change = document.getElementsByClassName("change");
+
 let colorCombo = mixBox.style.backgroundColor;
+
 
 let red = 0;
 let green = 0;
@@ -30,45 +33,50 @@ function startBox() {
 //1 div that shows current calculated color
 function mixboxText() {
 
-    mixBox.innerHTML= "Current Color: RGB";
-    
+    mixBox.innerHTML= "Current Color: " + mixBox.style.backgroundColor;
+    //console.log("updatetext");
 
 }
 
 startBox();
 mixboxText();
+
+
 //User press 9 buttons associated with RGB which will change black div's colors
 //Buttons should affect additively
 //9 buttons. +1, +5, and +10 for each color
-function RgbButt1() {
+function blueButtEvent(event) {
 
-    colorCombo + "#010000";
-    //mixBox.style.backgroundColor
+    //console.log(event.target.dataset.change);
+    
+    blue += Number(event.target.dataset.change);
+    let tmpcolor = "rgb("+red+","+green+","+ blue +")";
+    console.log(tmpcolor);
+
+    mixBox.style.backgroundColor= tmpcolor;
+    mixboxText();
+}
+
+function greenButtEvent(event) {
+
+    green += Number(event.target.dataset.change);
+    let tmpcolor = "rgb("+red+","+green+","+ blue +")";
+    console.log(tmpcolor);
+
+    mixBox.style.backgroundColor= tmpcolor;
+
+    mixboxText();
+}
+
+function redButtEvent(event) {
+
+    red += Number(event.target.dataset.change);
+    let tmpcolor = "rgb("+red+","+green+","+ blue +")";
+    console.log(tmpcolor);
+
+    mixBox.style.backgroundColor= tmpcolor;
+
+    mixboxText();
 }
 
 console.log(mixBox.style.backgroundColor);
-
-function RgbButt2() {
-
-    colorCombo + "#050000";
-
-}
-
-function RgbButt3() {
-
-    colorCombo + "#100000";
-
-}
-
-function rGbButt4() {
-
-    colorCombo + "#000100";
-
-}
-
-function rGbButt5() {
-
-    colorCombo + "#000500";
-
-}
-
