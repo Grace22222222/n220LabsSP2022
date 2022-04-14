93a7d09d9e701f6a3a9e159dfcbd2670adca60bc
Grace22222222 <gtpayne@iu.edu>
2022-04-13 22:18:56 -0400

    Finished two assignments

--- Lab/EventsEx/EventsEx1/app.js
+++ Lab/EventsEx/EventsEx1/app.js
@@ -4,18 +4,54 @@
 
 //Color Changer
 
-//Make three gray square divs. 200px by 200px, all floated to the left. Margin of 5px
 
-//Use only one event handler, and write event listeners to respond to a click on each element. Each element should change to a differeent color. One red, One green, One blue. 
-//Use a data attribute on elements to store the color to change to
 
 let colorBox1 = document.getElementById("colorBox1");
 let colorBox2 = document.getElementById("colorBox2");
 let colorBox3 = document.getElementById("colorBox3");
 
+let boxes = document.getElementsByClassName("box");
+
+cubeColor1(); 
+cubeColor2(); 
+cubeColor3();
+
+//Use only one event handler, and write event listeners to respond to a click on each element. Each element should change to a different color. One red, One green, One blue. 
+for(let i = 0; i < boxes.length; i++) {
+
+    boxes[i].addEventListener("click",function(){
+
+        buttonClick(i)
+
+    });
+    
+}
+
+
+function buttonClick(x) {
+
+    if (x == 0) {
+
+        buttonClick1();
+
+    }
+    if (x == 1) {
+
+        buttonClick2();
+
+    }
+    if (x == 2) {
+
+        buttonClick3();
+
+    }
+
+}
+
+//Make three gray square divs. 200px by 200px, all floated to the left. Margin of 5px
 function cubeColor1() {
 
-    colorBox1.style.backgroundColor= "#555555";
+    colorBox1.style.backgroundColor= colorBox1.dataset.color;
     colorBox1.style.height= "200px";
     colorBox1.style.width= "200px";
     colorBox1.style.margin= "5px";
@@ -23,10 +59,9 @@ function cubeColor1() {
 
 }
 
-
 function cubeColor2() {
 
-    colorBox2.style.backgroundColor= "#555555";
+    colorBox2.style.backgroundColor= colorBox2.dataset.color;
     colorBox2.style.height= "200px";
     colorBox2.style.width= "200px";
     colorBox2.style.margin= "5px";
@@ -36,7 +71,7 @@ function cubeColor2() {
 
 function cubeColor3() {
 
-    colorBox3.style.backgroundColor= "#555555";
+    colorBox3.style.backgroundColor= colorBox3.dataset.color;
     colorBox3.style.height= "200px";
     colorBox3.style.width= "200px";
     colorBox3.style.margin= "5px";
@@ -44,7 +79,22 @@ function cubeColor3() {
 
 }
 
-cubeColor1(); 
-cubeColor2(); 
-cubeColor3();
+//Use a data attribute on elements to store the color to change to
+function buttonClick1() {
+
+    colorBox1.dataset.color= "#990000";
+    cubeColor1();
+}
+function buttonClick2() {
+
+    colorBox2.dataset.color= "#009900";
+    cubeColor2();
+   
+}
+function buttonClick3() {
+
+    colorBox3.dataset.color= "#000099";
+    cubeColor3();
+
+}
 
--- Lab/EventsEx/EventsEx3/app.js
+++ Lab/EventsEx/EventsEx3/app.js
@@ -6,24 +6,24 @@
 
 //Application with 3 buttons that ask questions. 
 //Each button should have a data answer attribute that lists the answer
-//When button is clicked, display the answer to button's question in a div
-
 let questions = document.getElementById("questions");
 
 let elements = document.getElementsByClassName("QNA");
-/*
+
 for(let i = 0; i < elements.length; i++) {
 
-    elements[i].addEventListener("click",);
+    elements[i].addEventListener("click", function(){
+
+        buttonClick(i);
+
+    });
 
 }
-*/
 
 //Use only one function and that function must make use of the button's data attribute
-elements.addEventListener("click",buttonClick);
-
-function buttonClick() {
-
-    questions.innerHTML= elements;
+function buttonClick(x) {
+    
+//When button is clicked, display the answer to button's question in a div
+    questions.innerHTML= elements[x].dataset.answer;
 
 }
